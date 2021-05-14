331611858bf72598217d8509ec7f5cda728a32d8
gnez_dd <519364808@qq.com>
2021-05-15 01:35:06 +0800

    dd

---
+++ ddsPostBar/dao/codedao.go
@@ -0,0 +1,31 @@
+package dao
+
+import "ddsPostBar/utils"
+
+//FindCodeIdByEmail 根据邮箱查找id
+func FindCodeIdByEmail(email string)(id int64){
+	sqlStr := "select id from code where email = ?;"
+	row := utils.Db.QueryRow(sqlStr,email)
+	_ = row.Scan(&id)
+	return
+}
+
+//SetCodeByID 根据id修改code
+func SetCodeByID(id int64,code string){
+	sqlStr := "update code set code_num = ? where id = ?;"
+	_,_ = utils.Db.Exec(sqlStr,code,id)
+}
+
+//AddCode 新增邮箱
+func AddCode(email string,code string){
+	sqlStr := "insert into code (email,code_num) values (?,?);"
+	_,_ = utils.Db.Exec(sqlStr,email,code)
+}
+
+//FindCodeNumByEmail 根据邮箱查找验证码
+func FindCodeNumByEmail(email string)(code string){
+	sqlStr := "select code_num from code where email = ?;"
+	row := utils.Db.QueryRow(sqlStr,email)
+	_ = row.Scan(&code)
+	return
+}
